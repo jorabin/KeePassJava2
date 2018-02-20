3b5e359dc64ed4545d4107d0796d242545d00a09
Jo Rabin <jo@linguafranca.org>
2018-02-20 19:08:25 +0000

    Improve KDBX tests

--- kdbx/src/test/java/org/linguafranca/pwdb/kdbx/stream/KdbxSerializerTest.java
+++ kdbx/src/test/java/org/linguafranca/pwdb/kdbx/stream/KdbxSerializerTest.java
@@ -1,33 +1,27 @@
 package org.linguafranca.pwdb.kdbx.stream;
 
+import com.google.common.io.CharStreams;
 import com.google.common.io.LittleEndianDataInputStream;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.linguafranca.pwdb.hashedblock.HmacBlockInputStream;
-import org.linguafranca.pwdb.kdbx.KdbxCredentials;
 import org.linguafranca.pwdb.kdbx.KdbxCreds;
 import org.linguafranca.pwdb.kdbx.KdbxHeader;
 import org.linguafranca.pwdb.kdbx.KdbxSerializer;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.attribute.FileAttribute;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author jo
  */
 public class KdbxSerializerTest {
 
-    @Test
-    public void loadArgonheader() throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-AES-Argon2.kdbx");
-        KdbxHeader header = KdbxSerializer.readOuterHeader(inputStream, new KdbxHeader());
-        System.out.println("Version " + header.getVersion());
-    }
-
-    @Test
+    @Test @Ignore // can be used for detailed HMAC debugging
     public void getHmacStream() throws IOException {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-AES-Argon2.kdbx");
         KdbxHeader header = KdbxSerializer.readOuterHeader(inputStream, new KdbxHeader());
@@ -46,51 +40,39 @@ public class KdbxSerializerTest {
     public void loadAesheader() throws IOException {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-AES-AES.kdbx");
         KdbxHeader header = KdbxSerializer.readOuterHeader(inputStream, new KdbxHeader());
-        System.out.println("Version " + header.getVersion());
+        assertEquals(3, header.getVersion());
     }
 
     @Test
-    public void loadChaChaAesHeader() throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-ChaCha-AES.kdbx");
+    public void loadArgonheader() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-AES-Argon2.kdbx");
         KdbxHeader header = KdbxSerializer.readOuterHeader(inputStream, new KdbxHeader());
-        System.out.println("Version " + header.getVersion());
+        assertEquals(4, header.getVersion());
+    }
+
+
+    @Test
+    public void loadChaChaAesHeader() throws IOException {
+        String expected = CharStreams.toString(new InputStreamReader(KdbxSerializerTest.class.getClassLoader().getResourceAsStream("ChaCha-AES.xml")));
+        check("ChaCha-AES.xml","V4-ChaCha-AES.kdbx");
     }
 
     @Test
     public void getDecryptedArgonInputStream() throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-AES-Argon2.kdbx");
-        InputStream is = KdbxSerializer.createUnencryptedInputStream(new KdbxCreds("123".getBytes()), new KdbxHeader(), inputStream);
-
-        PrintWriter pw = new PrintWriter(new FileOutputStream(Files.createTempFile("temp", "xml").toFile()));
-        Scanner s = new Scanner(is);
-        while (s.hasNext()) {
-            pw.println(s.nextLine());
-        }
-        s.close();
-        pw.close();
+        String expected = CharStreams.toString(new InputStreamReader(KdbxSerializerTest.class.getClassLoader().getResourceAsStream("AES-Argon2.xml")));
+        check("AES-Argon2.xml","V4-AES-Argon2.kdbx");
     }
 
     @Test
     public void getDecryptedAesInputStream() throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-AES-AES.kdbx");
-        InputStream is = KdbxSerializer.createUnencryptedInputStream(new KdbxCreds("123".getBytes()), new KdbxHeader(), inputStream);
-        Scanner s = new Scanner(is);
-        while (s.hasNext()) {
-            System.out.println(s.nextLine());
-        }
-        s.close();
+        check("AES-AES.xml", "V4-AES-AES.kdbx");
     }
 
-    @Test
-    public void getDecryptedCHaChaInputStream() throws IOException {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("V4-ChaCha-AES.kdbx");
+    private void check(String compare, String file) throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(file);
         InputStream is = KdbxSerializer.createUnencryptedInputStream(new KdbxCreds("123".getBytes()), new KdbxHeader(), inputStream);
-        Scanner s = new Scanner(is);
-        while (s.hasNext()) {
-            System.out.println(s.nextLine());
-        }
-        s.close();
-
+        String actual = CharStreams.toString(new InputStreamReader(is, Charset.forName("UTF-8")));
+        String expected = CharStreams.toString(new InputStreamReader(KdbxSerializerTest.class.getClassLoader().getResourceAsStream(compare)));
+        assertEquals(expected, actual);
     }
-
 }
