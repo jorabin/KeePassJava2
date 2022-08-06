33f29f2f98166cb13f1e573d26a2cd500c7dbb4d
Noah G <95947224+count-of-monte-keystone@users.noreply.github.com>
2022-08-05 21:49:41 -0400

    Update KdbxStreamFormat.java
    Updated/added some methods to support saving KDBX4 databases, as well as provide more control over database saving in general.

--- kdbx/src/main/java/org/linguafranca/pwdb/kdbx/KdbxStreamFormat.java
+++ kdbx/src/main/java/org/linguafranca/pwdb/kdbx/KdbxStreamFormat.java
@@ -21,6 +21,7 @@ import org.linguafranca.pwdb.Credentials;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 /**
@@ -32,44 +33,55 @@ public class KdbxStreamFormat implements StreamFormat {
     private final Version version;
 
     public enum Version {
+    	//maybe change these to more accurately reflect version number
+    	//31=3.1, 40=4, 41=4.1, etc.
         KDBX31(3),
         KDBX4(4);
 
         private final int version;
 
         Version(int num) {
-            this.version = num;
+            ersion = num;
         }
 
         int getVersionNum() {
-            return this.version;
+            return version;
         }
     }
 
     /**
-     * Create a StreamFormat for reading or for writing v3
+     * Create a StreamFormat for reading or for writing KDBX4
      */
     public KdbxStreamFormat() {
-        this.version = Version.KDBX31;
+        version = Version.KDBX4;
     }
 
     /**
-     * Specify a version for writing
-     * @param version the version
+     * Create a StreamFormat for reading or writing a KDBX database of version
+     * @param version the version of the KDBX database
      */
     public KdbxStreamFormat(Version version) {
         this.version = version;
     }
 
     @Override
+    public int getStreamFormatVersion() {
+    	return version.getVersionNum();
+    }
+    
+    @Override
     public void load(SerializableDatabase serializableDatabase, Credentials credentials, InputStream encryptedInputStream) throws IOException {
-        KdbxHeader kdbxHeader = new KdbxHeader();
+        KdbxHeader kdbxHeader = new KdbxHeader(version.getVersionNum());
+        
         InputStream decryptedInputStream = KdbxSerializer.createUnencryptedInputStream(credentials, kdbxHeader, encryptedInputStream);
+       
         serializableDatabase.setEncryption(kdbxHeader.getStreamEncryptor());
         serializableDatabase.load(decryptedInputStream);
+        
         if (kdbxHeader.getVersion() == 3 && !Arrays.equals(serializableDatabase.getHeaderHash(), kdbxHeader.getHeaderHash())) {
             throw new IllegalStateException("Header hash does not match");
         }
+        
         if (kdbxHeader.getVersion() == 4) {
             int count = 0;
             for (byte[] binary: kdbxHeader.getBinaries()) {
@@ -77,20 +89,62 @@ public class KdbxStreamFormat implements StreamFormat {
                 count++;
             }
         }
+        
         decryptedInputStream.close();
     }
 
     @Override
-    public void save(SerializableDatabase serializableDatabase, Credentials credentials, OutputStream encryptedOutputStream) throws IOException {
-        // fresh kdbx header
+    /**
+     * Creates a default KDBX header based on StreamFormat version
+     * and saves to the corresponding formatted output stream
+     */
+    public void save(SerializableDatabase serializableDatabase, Credentials credentials, OutputStream unencryptedOutputStream) throws IOException {
+        // fresh default KDBX3.1/4.x header
         KdbxHeader kdbxHeader = new KdbxHeader(version.getVersionNum());
-        OutputStream unencrytedOutputStream = KdbxSerializer.createEncryptedOutputStream(credentials, kdbxHeader, encryptedOutputStream);
+        OutputStream kdbxOutputStream = null;
+        try {
+			kdbxOutputStream = KdbxSerializer.createKdbxFormattedOutputStream(credentials, kdbxHeader, unencryptedOutputStream);
+		} catch (NoSuchAlgorithmException | IOException e) {
+			e.printStackTrace();
+		}
+
         if (version == Version.KDBX31) {
             serializableDatabase.setHeaderHash(kdbxHeader.getHeaderHash());
         }
+        
         serializableDatabase.setEncryption(kdbxHeader.getStreamEncryptor());
-        serializableDatabase.save(unencrytedOutputStream);
-        unencrytedOutputStream.flush();
-        unencrytedOutputStream.close();
+        serializableDatabase.save(kdbxOutputStream);
+        
+        kdbxOutputStream.flush();
+        kdbxOutputStream.close();
+    }
+    
+    /**
+     * Uses a user-defined KDBX header to appropriately format an output stream
+     * for writing the database to.
+     * @param customKdbxHeader
+     * @param serializableDatabase
+     * @param credentials
+     * @param unencryptedOutputStream
+     * @throws IOException
+     */
+    public void save(KdbxHeader customKdbxHeader, SerializableDatabase serializableDatabase, Credentials credentials, OutputStream unencryptedOutputStream) throws IOException {
+        OutputStream kdbxOutputStream = null;
+        try {
+			kdbxOutputStream = KdbxSerializer.createKdbxFormattedOutputStream(credentials, customKdbxHeader, unencryptedOutputStream);
+		} catch (NoSuchAlgorithmException | IOException e) {
+			e.printStackTrace();
+		}
+
+        if (version == Version.KDBX31) {
+            serializableDatabase.setHeaderHash(customKdbxHeader.getHeaderHash());
+        }
+        
+        serializableDatabase.setEncryption(customKdbxHeader.getStreamEncryptor());
+        serializableDatabase.save(kdbxOutputStream);
+        
+        kdbxOutputStream.flush();
+        kdbxOutputStream.close();
+    	
     }
 }
