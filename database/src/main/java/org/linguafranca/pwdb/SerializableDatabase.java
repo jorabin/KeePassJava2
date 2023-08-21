1fae548b9c1825714f7f6c78d59472d7b9cdb7bd
Jo Rabin <jo@linguafranca.org>
2023-08-21 17:32:55 +0100

    Javadoc correction

--- database/src/main/java/org/linguafranca/pwdb/SerializableDatabase.java
+++ database/src/main/java/org/linguafranca/pwdb/SerializableDatabase.java
@@ -25,15 +25,15 @@ import java.io.OutputStream;
 /**
  * This interface allows for serialization and deserialization of databases, which is
  * theoretically decoupled from the actual format they use for serialization.
- * <p/>
+ * <p>
  * Databases instantiate themselves from a stream and serialize to a stream,
  * and need to be able to encrypt and decrypt data (e.g. Protected fields in KDBX format).
- * <p/>
+ * <p>
  * KDBX V3 databases contain a header hash (i.e. a hash of the contents of
  * some portion of the {@link StreamFormat} they have been loaded from or saved to).
  * Which means that databases must support the setting of this value after the header
  * has been written on save, and reading the value after load to allow for integrity checking.
- * <p/>
+ * <p>
  * KDBX V4 databases have their attachments in the header so databases need to support setting
  * and getting of attachments for serialization
  */
